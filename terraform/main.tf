0499c1aa4778e60bff080d6cc33e922210eba0a5
PadminiLala <plala0901@gmail.com>
2024-10-01 17:10:33 -0400

    adding dev account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,31 @@ module "sandbox_aft" {
 
   custom_fields = {
     group = "non-prod"
-    vpc = "delete_default_vpc"
-    test_vpc_delete = "mustbetrue"
   }
 
-  account_customizations_name = "sandbox"
+}
+module "dev_aft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "padmini.lala+dev_aft@hilton.com"
+    AccountName               = "dev_aft"
+    ManagedOrganizationalUnit = "sandbox"
+    SSOUserEmail              = "padmini.lala+dev_aft@hilton.com"
+    SSOUserFirstName          = "padmini"
+    SSOUserLastName           = "dev_aft"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Padmini"
+    change_reason       = "Learn AFT Control Tower Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
 }
