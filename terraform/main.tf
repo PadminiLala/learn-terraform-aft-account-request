cafeb38e841e31b3527ba605d38931778a69bce5
PadminiLala <plala0901@gmail.com>
2025-02-13 23:03:37 -0500

    networktest

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,3 +52,30 @@ module "configtest" {
 
   account_customizations_name = "configtest"
 }
+module "networktest" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "padmini.lala+networktest@hilton.com"
+    AccountName               = "networktest"
+    ManagedOrganizationalUnit = "sandbox"
+    SSOUserEmail              = "padmini.lala+networktest@hilton.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Padmini"
+    change_reason       = "Learn AFT Control Tower Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "networktest"
+}
